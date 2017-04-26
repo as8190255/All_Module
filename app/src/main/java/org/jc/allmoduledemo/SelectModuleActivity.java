484d8f4b4a3a4dcb1f225950e2a2a83270cf1ed9
as8190255 <355225537@qq.com>
2017-04-26 18:03:55 +0800

    首次

---
+++ app/src/main/java/org/jc/allmoduledemo/SelectModuleActivity.java
@@ -0,0 +1,21 @@
+package org.jc.allmoduledemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.jc.allmoduledemo.widget.recyclerview.RecyclerviewActivity;
+
+/**
+ * 首页,选择要测试的模块对应的界面
+ */
+public class SelectModuleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_module);
+
+        startActivity(new Intent(this, RecyclerviewActivity.class));
+    }
+}
---
+++ app/src/main/java/org/jc/allmoduledemo/widget/recyclerview/RecyclerviewActivity.java
@@ -0,0 +1,18 @@
+package org.jc.allmoduledemo.widget.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.jc.allmoduledemo.R;
+
+/**
+ * RecyclerView 基本用法
+ */
+public class RecyclerviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview);
+    }
+}
